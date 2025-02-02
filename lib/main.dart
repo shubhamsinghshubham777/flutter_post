974badd8e1fd4914c93b865f94661b087ee89912
Shubham Singh <shubhamsinghshubham777@gmail.com>
2025-02-02 18:49:55 +0530

    Fix outdated Android dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_post/features/authentication/provider/authentication_provider.dart';
@@ -8,19 +6,16 @@ import 'package:flutter_post/features/posts/view/dashboard_screen.dart';
 import 'package:flutter_post/generated/hive_registrar.g.dart';
 import 'package:flutter_post/utils/utils.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive_ce/hive.dart';
+import 'package:hive_ce_flutter/adapters.dart';
 
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await _setupFirebase();
-  onPlatformOrNull(
-    web: () => Hive..registerAdapters(),
-    orElse: () => Hive
-      ..init(Directory.current.path)
-      ..registerAdapters(),
-  );
+  Hive
+    ..initFlutter()
+    ..registerAdapters();
   runApp(const ProviderScope(child: FlutterPostApp()));
 }
 
